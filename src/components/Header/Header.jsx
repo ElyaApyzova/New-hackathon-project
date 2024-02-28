0d2b702975bb27df8d3baaddc3d5326068a107fc
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-02-28 21:36:13 +0300

    changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import {  Link as RouterLink  } from "react-router-dom";
-import { AppBar, Container, Toolbar, Box, Stack, IconButton, Menu, MenuItem, ListItemIcon, Link  } from '@mui/material'
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { AppBar, Container, Toolbar, Box, Stack, IconButton, Menu, MenuItem, ListItemIcon, Link, Typography } from '@mui/material';
 import Language from '../../assets/language.png';
 import Logo from '../../assets/Logo.png';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -11,119 +10,114 @@ import PersonIcon from '@mui/icons-material/Person';
 import MemoryIcon from '@mui/icons-material/Memory';
 import FamilyRestroomIcon from '@mui/icons-material/FamilyRestroom';
 
-
-
-
 const Header = () => {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-    
-  
-    const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-   
+    const [language, setLanguage] = useState('ru');
 
-  return (
-    <AppBar position='static' sx={{ 
-        background: '#A0D206',  
-        color:'#000000',}}>
-        <Container maxWidth="xl">
-       <Toolbar>
-        <img src={Logo}  edge='start'  aria-label="logo" sx={{ width: '85px', height: '66px'}}  />
-        <Typography variant='h6' component='div'  sx={{ flexGrow: 1, color: '#0E0E0E'}}>Семейный сад</Typography>
-        
-       
-         
-        <Stack direction='row' spacing={4}  marginRight={12}   sx={{  fontSize: '18px',  display: { xs: 'none', md: 'flex' }}}>
-            
-        <Link component={RouterLink} to="/Home" sx={{ textDecoration: 'none', color: '#0E0E0E'  }}>Главное
-        </Link>
-        
-        <Link  component={RouterLink}  to="/Tree" sx={{ textDecoration: 'none', color: '#0E0E0E'}}>Деревья 
-        </Link>
-        <Link  component={RouterLink}  to="/About us" sx={{ textDecoration: 'none', color: '#0E0E0E'}}>О нас
-        </Link>
-        <Link  component={RouterLink}  to="/Memories" sx={{ textDecoration: 'none', color: '#0E0E0E'}}>Воспоминания
-        </Link>
-        <Link  component={RouterLink}  to="/Family tree" sx={{ textDecoration: 'none', color: '#0E0E0E'}}>Семейное древо
-        </Link> 
-        </Stack>  
-        
-        <Stack direction='row' spacing={1}>
-  <img src={Language} alt='language' />
-  <Typography variant='body2'>Русский</Typography>
-</Stack>
-        <Box edge='end' sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-                 <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-                                    <MenuItem  to="/" >
-                                        <ListItemIcon>
-                                            <HomeIcon />
-                                        </ListItemIcon>
-                                        Главное
-                                    </MenuItem>
-                                    <MenuItem  to="/Trees" >
-                                        <ListItemIcon>
-                                        <NaturePeopleIcon />
-                                        </ListItemIcon>
-                                        Деревья
-                                    </MenuItem>
-                                    <MenuItem  to="/About us" >
-                                        <ListItemIcon>
-                                            <PersonIcon />
-                                        </ListItemIcon>
-                                        О нас
-                                    </MenuItem>
-                                    <MenuItem  to="/Memories" >
-                                        <ListItemIcon>
-                                            <MemoryIcon />
-                                        </ListItemIcon>
-                                        Воспоминания
-                                    </MenuItem>
-                                    <MenuItem  to="/FamilyTree" >
-                                        <ListItemIcon>
-                                            <FamilyRestroomIcon/>
-                                        </ListItemIcon>
-                                        Семейное древо
-                                    </MenuItem>
-                                </Menu>
-          </Box>
-       </Toolbar>
-       </Container>
-    </AppBar>
-  );
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleLanguageChange = () => {
+        if (language === 'ru') {
+            setLanguage('kg'); // Change to Kyrgyz
+        } else {
+            setLanguage('ru'); // Change back to Russian
+        }
+    };
+
+    return (
+        <AppBar position='static' sx={{
+            background: '#A0D206',
+            color: '#000000',
+        }}>
+            <Container maxWidth="xl">
+                <Toolbar>
+                    <img src={Logo} edge='start' aria-label="logo" sx={{ width: '85px', height: '66px' }} />
+                    <Typography variant='h6' component='div' sx={{ flexGrow: 1, color: '#0E0E0E' }}>Семейный сад</Typography>
+
+                    <Stack direction='row' spacing={4} marginRight={12}  sx={{ fontSize: '18px', cursor: 'pointer', display: { xs: 'none', md: 'flex' } }}>
+                        <Link component={RouterLink} to="/" sx={{ textDecoration: 'none', color: '#0E0E0E' }}>Главное</Link>
+                        <Link component={RouterLink} to="/Tree" sx={{ textDecoration: 'none', color: '#0E0E0E' }}>Деревья</Link>
+                        <Link component={RouterLink} to="/About us" sx={{ textDecoration: 'none', color: '#0E0E0E' }}>О нас</Link>
+                        <Link component={RouterLink} to="/Memories" sx={{ textDecoration: 'none', color: '#0E0E0E' }}>Воспоминания</Link>
+                        <Link component={RouterLink} to="/Family tree" sx={{ textDecoration: 'none', color: '#0E0E0E' }}>Семейное древо</Link>
+                    </Stack>
+
+                    <Stack direction='row' spacing={1} alignItems="center" cursor='pointer' onClick={handleLanguageChange} sx={{ cursor: 'pointer' }}>
+                        <img src={Language} alt={language === 'ru' ? 'Russian language' : 'Kyrgyz language'} />
+                        <Typography sx={{ fontSize: '14px' }}>{language === 'ru' ? 'Русский' : 'Кыргызча'}</Typography>
+                    </Stack>
+
+                    <Box edge='end' sx={{ display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            <MenuItem component={RouterLink} to="/">
+                                <ListItemIcon>
+                                    <HomeIcon />
+                                </ListItemIcon>
+                                Главное
+                            </MenuItem>
+                            <MenuItem component={RouterLink} to="/Trees">
+                                <ListItemIcon>
+                                    <NaturePeopleIcon />
+                                </ListItemIcon>
+                                Деревья
+                            </MenuItem>
+                            <MenuItem component={RouterLink} to="/About us">
+                                <ListItemIcon>
+                                    <PersonIcon />
+                                </ListItemIcon>
+                                О нас
+                            </MenuItem>
+                            <MenuItem component={RouterLink} to="/Memories">
+                                <ListItemIcon>
+                                    <MemoryIcon />
+                                </ListItemIcon>
+                                Воспоминания
+                            </MenuItem>
+                            <MenuItem component={RouterLink} to="/FamilyTree">
+                                <ListItemIcon>
+                                    <FamilyRestroomIcon />
+                                </ListItemIcon>
+                                Семейное древо
+                            </MenuItem>
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 };
 
 export default Header;
