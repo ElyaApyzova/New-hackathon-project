f05df9fd853babc81f9d2d589c5bc912639f32d0
LenoMac <hafizfullstack@gmail.com>
2024-03-01 18:15:40 +0600

    from hafiz

---
+++ src/components/MainPage/Main.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Container, Typography, Button } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import main from "../../assets/main.png";
+
+export default function Main() {
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        gap: "50px",
+        marginTop: "30px",
+      }}
+    >
+      <Grid sx={{ display: "flex", flexDirection: "column", gap: "50px" }}>
+        <Grid sx={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
+          <Typography variant="h3" sx={{ fontWeight: 600 }}>
+            Создайте свое “Семейное древо” вместе с нами.
+          </Typography>
+          <Typography sx={{ fontWeight: 400, fontSize: "30px" }} component="p">
+            Сохраним и приумножим наши леса
+          </Typography>
+        </Grid>
+        <Button
+          sx={{
+            background: "#A0D206",
+            width: "max-content",
+            padding: 2,
+            color: "white",
+            fontWeight: 600,
+          }}
+        >
+          Создать мое дерево
+        </Button>
+      </Grid>
+      <Grid>
+        <img style={{ width: 1100 }} src={main} />
+      </Grid>
+    </Container>
+  );
+}
--- src/components/MyAppBar/MyAppBar.jsx
+++ src/components/MyAppBar/MyAppBar.jsx
@@ -1,40 +1,66 @@
-import React from 'react';
-import { AppBar, Toolbar, InputBase, Typography, Link, InputAdornment, Stack } from '@mui/material';
-import arrow from '../../assets/arrow.png';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  InputBase,
+  Typography,
+  Link,
+  InputAdornment,
+  Stack,
+} from "@mui/material";
+import arrow from "../../assets/arrow.png";
 import { Link as RouterLink } from "react-router-dom";
-import SearchIcon from '@mui/icons-material/Search';
-
-
+import SearchIcon from "@mui/icons-material/Search";
 
 const MyAppBar = () => {
-
-
   return (
-    <AppBar position="static" sx={{backgroundColor:' #fff', boxShadow: 'none'}}>
-      <Toolbar style={{ justifyContent: 'flex-end' }}>
-        <div >
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: " #fff", boxShadow: "none" }}
+    >
+      <Toolbar style={{ justifyContent: "flex-end" }}>
+        <div>
           <div>
-          
-            <InputBase  sx={{borderColor:'#000', borderWidth:'1px' , borderRadius:'25px' , paddingLeft:'7px'}} 
-            
-            placeholder="Поиск"
-            startAdornment={
-              <InputAdornment position="start">
-                <SearchIcon style={{ color: 'black' }} />
-              </InputAdornment>
-            }
-            inputProps={{ 'aria-label': 'search' }}
-          />
+            <InputBase
+              sx={{
+                borderColor: "#000",
+                borderWidth: "1px",
+                borderRadius: "25px",
+                paddingLeft: "7px",
+              }}
+              placeholder="Поиск"
+              startAdornment={
+                <InputAdornment position="start">
+                  <SearchIcon style={{ color: "black" }} />
+                </InputAdornment>
+              }
+              inputProps={{ "aria-label": "search" }}
+            />
           </div>
-        
         </div>
-        <div style={{marginLeft:'50px'}} >
-        <Typography variant="body1"  component="div">
-        <Stack direction='row' spacing={0}  marginRight={12}   sx={{  fontSize: '18px',  display: { xs: 'none', md: 'flex' }}}>
-        <Link component={RouterLink} to="#" sx={{ textDecoration: 'none', color: '#0E0E0E',  }}>Войти</Link>
-        <img src={arrow}  edge='start'  aria-label="logo" sx={{ width: '17px', height: '16px'}}      />
-        </Stack>
-        </Typography>
+        <div style={{ marginLeft: "50px" }}>
+          <Typography variant="body1" component="div">
+            <Stack
+              direction="row"
+              spacing={0}
+              marginRight={12}
+              sx={{ fontSize: "18px", display: { xs: "none", md: "flex" } }}
+            >
+              <Link
+                component={RouterLink}
+                to="#"
+                sx={{ textDecoration: "none", color: "#0E0E0E" }}
+              >
+                Войти
+              </Link>
+              <img
+                src={arrow}
+                edge="start"
+                aria-label="logo"
+                sx={{ width: "17px", height: "16px" }}
+              />
+            </Stack>
+          </Typography>
         </div>
       </Toolbar>
     </AppBar>
