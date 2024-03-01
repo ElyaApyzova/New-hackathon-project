73365474e96faef7e415750750ba49d2f8994609
LenoMac <hafizfullstack@gmail.com>
2024-03-01 17:28:27 +0600

    dfgdf

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,34 @@
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MyAppBar from "./MyAppBar/MyAppBar";
+import Image from "../assets/main.png";
+import Grid from "@mui/material/Grid";
 
 const Home = () => {
-    return (
-  <div>
-
+  return (
     <div>
-    <MyAppBar/>
-    </div>
       <div>
-       <h1>Hello</h1> 
+        <MyAppBar />
       </div>
-  </div>
-    );
-  };
-  
-  export default Home;
+      <Grid
+        sx={{
+          margin: "auto",
+          width: 1200,
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Grid className="flex flex-col">
+          <h1>Создайте свое “Семейное древо” вместе с нами.</h1>
+          <p>Сохраним и приумножим наши леса</p>
+          <button className="bg-green-600">Создать мое дерево</button>
+        </Grid>
+        <Grid>
+          <img src={Image} alt="" />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Home;
